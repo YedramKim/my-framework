a483bcbe39a5db6e50cf957b02ecbc18897191db
YedarmKim <oncoder@naver.com>
2018-03-07 23:41:16 +0900

    여기까지~~~~

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,35 +1,69 @@
 class Scheduler {
 	constructor (config, props) {
-		const moment = require('moment-timezone');
-		this.config = {
-			serviceLocalTimezone: moment.tz.zone(config.local),
-			serverTimezone: (new Date()).getTimezoneOffset()
-		};
+		this.config = config;
 		this.props = props;
 	}
 
 	async onScheduler () {
+		const moment = require('moment-timezone');
 		const path = require('path');
 		const fse = require('fs-extra');
+
+		const timezoneOffsets = {
+			serviceLocal: moment.tz.zone(config.local),
+			server: (new Date()).getTimezoneOffset()
+		};
 
 		const schedulesPath = path.join(__dirname, 'schedules', process.env.PRODUCT);
 		this.schedules = (await fse.readdir(schedulesPath)).filter(schedule => /\.js$/.test(schedule)).map(schedule => require(`${schedulesPath}/${schedule}`));
 
 		await this._launchSchedules();
 
-		this.scheduleWorkers = this.schedules.map(schedule => new ScheduleWorker(schedule, this.config, this.props));
+		this.scheduleWorkers = this.schedules.map(schedule => new ScheduleWorker(schedule, this.props, timezoneOffsets));
 
 		this.scheduleWorkers.forEach(scheduleWorker => {
-			scheduleWorker.working();
+			scheduleWorker.startWorking(timezoneOffsets).catch((err) => {
+				console.log('스케쥴러 시작 에러가 발생했습니다.');
+			});
 		});
 	}
 }
 
 class ScheduleWorker {
-	constructor (schedule, config, props) {
+	constructor (schedule, props, timezoneOffsets) {
+		this.scheduleData = schedule;
+		this.work = schedule.work;
+		this.props = props;
+		this.timezoneOffsets = timezoneOffsets;
+	}
+
+	async startWorking(timezoneOffsets) {
+		if (schedule.baseFromZeroTime) {
+			this.interval = schedule.interval;
+		} else {
+			this.interval = schedule.interval;
+		}
+
+		this.scheduleTimer = setTimeout(() => {
+			this.working();
+		}, this.interval);
+		await this.work(this.props);
 	}
 
 	async working() {
+		this._resetTimer();
+	}
+
+	_resetTimer () {
+		if (this.baseFromZeroTime) {
+			setTimeout(() => {
+				this.working();
+			}, 24 * 60 * 60 * 1000);
+		} else {
+			setTimeout(() => {
+				this.working();
+			}, this.interval);
+		}
 	}
 }
 module.exports = Scheduler;
--- scheduler/schedules/study/study.js
+++ scheduler/schedules/study/study.js
@@ -1,6 +1,6 @@
 const schedule = module.exports = {};
 
-schedule.hourInterval = 23;
+schedule.interval = 23;
 
 // 이것을 사용하면 0시 이후를 간격으로 돌게 한다.
 schedule.baseFromZeroTime = true;
