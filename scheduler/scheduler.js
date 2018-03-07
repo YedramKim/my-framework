af107e9b0d813a002ee1d0c4ce036d6d45077cd8
YedarmKim <oncoder@naver.com>
2018-03-07 23:19:48 +0900

    아아아아아아

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,6 +1,10 @@
 class Scheduler {
-	constructor (options, props) {
-		this.options = options;
+	constructor (config, props) {
+		const moment = require('moment-timezone');
+		this.config = {
+			serviceLocalTimezone: moment.tz.zone(config.local),
+			serverTimezone: (new Date()).getTimezoneOffset()
+		};
 		this.props = props;
 	}
 
@@ -13,53 +17,19 @@ class Scheduler {
 
 		await this._launchSchedules();
 
-		this.scheduleWorkers = this.schedules.map(schedule => new ScheduleWorker(schedule, this.options, this.props));
+		this.scheduleWorkers = this.schedules.map(schedule => new ScheduleWorker(schedule, this.config, this.props));
 
 		this.scheduleWorkers.forEach(scheduleWorker => {
-			scheduleWorker.startWork();
+			scheduleWorker.working();
 		});
-	}
-
-	async _launchSchedules () {
-		let schedulePromises = this.schedules.map(schedule => {
-			const result = schedule.work();
-			if (result instanceof Promise) {
-				return result;
-			} else {
-				return Promise.resolve(result);
-			}
-		});
-
-		return Promise.all(schedulePromises);
 	}
 }
 
 class ScheduleWorker {
-	constructor (schedule, options, props) {
-		this.hourInterval = schedule.hourInterval;
-		this.setScheduler = schedule.baseFromZeroTime ? setTimerNextDay : setTimerNextHours;
-		this.work = schedule.work;
-
-		this.work();
+	constructor (schedule, config, props) {
 	}
 
-	async triggerWork() {
-		if (this.scheduleTimer !== undefined) {
-			clearInterval(this.scheduleTimer);
-		}
+	async working() {
 	}
 }
-
-const setTimerNextDay = function() {
-	this.scheduleTimer = setInterval(() => {
-		this.working();
-	}, 24 * 60 * 60 * 1000);
-};
-
-const setTimerNextHours = function() {
-	this.scheduleTimer = setInterval(() => {
-		this.working();
-	}, schedule.hourInterval * 60 * 60 * 1000);
-};
-
 module.exports = Scheduler;
--- scheduler/schedules/study/study.js
+++ scheduler/schedules/study/study.js
@@ -5,6 +5,6 @@ schedule.hourInterval = 23;
 // 이것을 사용하면 0시 이후를 간격으로 돌게 한다.
 schedule.baseFromZeroTime = true;
 
-schedule.work = async (props, timeData) => {
+schedule.work = async (props) => {
 	console.log('지금은 23시 입니다');
 };
